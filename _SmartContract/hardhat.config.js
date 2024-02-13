eb0ac8c66f79b644dbd331ecd09f081394389891
Andreas Kobal <kobalandrey@gmail.com>
2024-02-13 03:05:34 +0000

    add contract

---
+++ _SmartContract/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
+
+const privateKey = process.env.PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: [privateKey]
+    }
+  },
+};
+
